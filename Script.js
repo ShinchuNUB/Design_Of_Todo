beeceeb2a4bcc9ba94f8e1400e48ae078f9dfb17
ShinchuNUB <viveknathani181@gmail.com>
2022-01-28 13:13:45 +0530

    Changes Applied..!

--- Script.js
+++ Script.js
@@ -1,4 +1,6 @@
-let arr = [];
+let arr = [undefined];
+let checkedarr = [];
+let cnt = 0;
 let isFromEdit = 0;
 let GlobalIndex = -1;
 var Todoname = document.getElementById("todo_name");
@@ -12,28 +14,50 @@ function AddValue()
 }
 function PushValue()
 {	
-	Todoname.value == "" ? alert("Null Not Allowed..!") : arr.push(Todoname.value);	
+	Todoname.value.trim() == "" ? alertify.alert("TextBox Is Empty...!","Sorry..! You Have To Enter Task In Box..!") : alertWithpushValue();
+
+}
+function alertWithpushValue()
+{
+	//alertify.alert("Nice One...!","Task Is Added Successfully...!");
+	arr.push(Todoname.value);
 }
 function EditValue()
 {
 	arr[GlobalIndex] = Todoname.value;
+	alertify.alert("Changes May Applied...!","Your Task Is Updated Successfuly...!");
 	btn.value = "+ Add";
 	isFromEdit = 0;
 }
 function justCall(index)
 {
-	confirm(`Are you Sure You Want To Delete ${arr[index]}..?`) ? delete arr[index] : "";
-	displayDetails();	
+	alertify.confirm('Deleting Task...!',`Are you Sure You Want To Delete ${arr[index]}..?`, function(){ arr[index]=undefined; displayDetails()}
+	, function(){ "" });
 }
 function displayDetails()
 {
+	Todoname.value= "";
+	btn.value="+ Add";
 	var tag = document.getElementById("todocontainer");
 	tag.innerHTML = '';
-	arr = arr.filter(val => val != undefined)
 	for(let increment=0; increment<arr.length; increment++)
 	{
-		tag.innerHTML += '<div class="list" id="list">	<input type="checkbox" id="'+increment+'" onClick="CheckTask('+increment+')"/> <input type="text" id="'+arr[increment]+'" value="'+arr[increment]+'" disabled />	<button class="edit" onClick="editTask('+increment+')"><i class="fa fa-edit" style="color:white; font-size: 25px;"></i></button><button class="delete" id="" onClick="justCall('+increment+')"><i class="fa fa-trash" style="color:white; font-size: 25px;"></i></button></div>';
+		letssee(increment) > 0 && arr[increment] != undefined
+		?
+		tag.innerHTML += '<div class="list" id="list">	<input type="checkbox" id="'+increment+'" onClick="CheckTask('+increment+')" checked/> <input type="text" class="TaskDone" id="'+arr[increment]+'" value="'+arr[increment]+'" disabled />	<button class="edit" onClick="editTask('+increment+')"><i class="fa fa-edit" style="color:white; font-size: 25px;"></i></button><button class="delete" id="" onClick="justCall('+increment+')"><i class="fa fa-trash" style="color:white; font-size: 25px;"></i></button></div>'
+		: 
+		arr[increment] != undefined 
+		?
+		tag.innerHTML += '<div class="list" id="list">	<input type="checkbox" id="'+increment+'" onClick="CheckTask('+increment+')"/> <input type="text" id="'+arr[increment]+'" value="'+arr[increment]+'" disabled />	<button class="edit" onClick="editTask('+increment+')"><i class="fa fa-edit" style="color:white; font-size: 25px;"></i></button><button class="delete" id="" onClick="justCall('+increment+')"><i class="fa fa-trash" style="color:white; font-size: 25px;"></i></button></div>'
+		:
+		tag.innerHTML += "";
+		;
 	}
+}
+function letssee(index)
+{
+	cnt = 0;
+	return checkedarr.filter(val => val == index, cnt++);
 }
 function editTask(index)
 {
@@ -47,6 +71,20 @@ function CheckTask(index)
 {
 	let checkbox = document.getElementById(index);
 	let thm = document.getElementById(arr[index]);
-	checkbox.checked == true ? alert(`Congrats...! You Done Your ${arr[index]}`) : "";
+	checkbox.checked == true ? alertify.alert('Keep It Up...!',`Congrats...! You Done Your ${arr[index]}...!`) : "";
 	checkbox.checked == true ? thm.classList.add("TaskDone") : thm.classList.remove("TaskDone");
+	checkedarr.push(index);
+	}
+
+//For the "ENTER" keypress button event
+Todoname.addEventListener("keyup", function (event) {
+    let enKey = event.which;
+    if (enKey === 13) {
+        document.getElementById("addBtn").click();
+    }
+});
+
+function DelAll()
+{
+	location.reload();
 }
